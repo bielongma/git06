e8c57a15b9b33c8e18716026660e7309f9482437
atguigu <atguigu@atguigu.com>
2021-10-29 15:09:01 +0800

    hot-fix commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("床前明月光");
         System.out.println("test02");
+        System.out.println("hot-fix");
     }
 }
